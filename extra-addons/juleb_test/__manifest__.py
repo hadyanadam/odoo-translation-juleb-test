90b38236016256b874cd19a5188cc8381010ce64
Hadyan Adam <hadyanadamn@gmail.com>
2022-08-29 15:31:54 +0700

    init commit - added docker-compose and odoo module for juleb testing propose

---
+++ extra-addons/juleb_test/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name': 'Juleb Test',
+    'version': '13.0.1.0.0',
+    'summary': 'Juleb Test',
+    'description': "Juleb Test",
+    'sequence': 101,
+    'depends': ['stock'],
+    'category': 'Operations/Inventory',
+    'data': [
+        'data/data.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
